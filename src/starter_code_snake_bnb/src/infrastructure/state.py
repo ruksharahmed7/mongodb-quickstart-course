af589be76c651beb8a9ebf6a31fa1a92b42e5967
rukshar69 <ruksharalam7@gmail.com>
2023-02-24 16:32:52 +0600

    done w/ tut

--- src/starter_code_snake_bnb/src/infrastructure/state.py
+++ src/starter_code_snake_bnb/src/infrastructure/state.py
@@ -1,4 +1,6 @@
-active_account = None
+from data.owners import Owner
+import services.data_service as svc
+active_account: Owner = None
 
 
 def reload_account():
@@ -6,5 +8,4 @@ def reload_account():
     if not active_account:
         return
 
-    # TODO: pull owner account from the database.
-    pass
+    active_account = svc.find_account_by_email(active_account.email)
